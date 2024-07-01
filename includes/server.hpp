6995f095e3f17272490b80c5f346ee95fdc3a324
11907223 <e.vaniterson98@gmail.com>
2024-07-01 20:57:09 +0200

    add version macro

--- includes/server.hpp
+++ includes/server.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: evan-ite <evan-ite@student.42.fr>          +#+  +:+       +#+        */
+/*   By: elisevaniterson <elisevaniterson@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 16:14:51 by evan-ite          #+#    #+#             */
-/*   Updated: 2024/07/01 16:39:27 by evan-ite         ###   ########.fr       */
+/*   Updated: 2024/07/01 20:56:44 by elisevanite      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 # include <iostream>
 # include <iomanip>
 # include <cstring>
+# include "../includes/Response.hpp"
+
+# define HTTPVERSION "HTTP/1.1"
 
 std::string find_key(std::string file_str, std::string keyword, char separator);
 
