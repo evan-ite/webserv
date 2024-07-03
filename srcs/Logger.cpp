2138849d3ab7289bed32ebde89b23b3f277d622c
Jan Strozyk <jstrozyk@students.42berlin.de>
2024-07-03 17:20:49 +0200

    add colors to logger

--- srcs/Logger.cpp
+++ srcs/Logger.cpp
@@ -4,7 +4,24 @@ loglevel_e loglevel = LOGLEVEL;
 
 Logger::Logger(loglevel_e _loglevel)
 {
-	this->_buffer << _loglevel << " : ";
+	switch (_loglevel)
+	{
+		case logERROR:
+			_buffer << ANSI_RED;
+			break;
+		case logWARNING:
+			_buffer << ANSI_YELLOW;
+			break;
+		case logINFO:
+			_buffer << ANSI_GREEN;
+			break;
+		case logDEBUG:
+			_buffer << ANSI_BLUE;
+			break;
+		default:
+			break;
+	}
+	_buffer << _loglevel << " : " << ANSI_RESET;
 }
 
 Logger::~Logger()
