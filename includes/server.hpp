04bbdd87f16bf5689056add1af8bb3525c3cabd3
elise <e.vaniterson98@gmail.com>
2024-07-03 14:26:57 +0200

    add read file to string func to utils

--- includes/server.hpp
+++ includes/server.hpp
@@ -6,12 +6,18 @@
 # include <iomanip>
 # include <cstring>
 # include <sstream>
+# include <fstream>
 # include <ctime>
 # include "../includes/Response.hpp"
+# include "../includes/Response.hpp"
+# include "../includes/Request.hpp"
+# include "../includes/Logger.hpp"
+
 
 # define HTTPVERSION "HTTP/1.1"
 
 std::string findKey(std::string file_str, std::string keyword, char separator);
 std::string splitReturnFirst(const std::string& str, const std::string& delimiter);
+std::string readFileToString(const std::string& filename);
 
 #endif
