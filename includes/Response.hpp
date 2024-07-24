f63b0c0b3b71e57a70cb64e14fecdb7405cc3185
ubazzane <urbanobazz@gmail.com>
2024-07-24 16:11:53 +0200

    Delete request ongoing

--- includes/Response.hpp
+++ includes/Response.hpp
@@ -40,7 +40,7 @@ class Response
 		void		postMethod(Request &request);
 		void		createFiles(Request &request, int &status);
 		void		getMethod(Request request, Server serverData, std::string root, std::string index);
-		void		deleteMethod(Request &request);
+		void		deleteMethod(Request &request, std::string root);
 		Location	findLoc(Request request, Server serverData);
 
 		int	_status;
